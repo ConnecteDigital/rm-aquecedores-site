c4d5cfa544289536cd8b7b470dd9ab1aa9e20d38
ConnecteDigital <connectdigital0305@gmail.com>
2025-08-16 10:17:57 -0300

    Atualizar imagem de fundo do hero section

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import logoRM from './assets/rm_aquecedores_logo.png';
 import instalacaoImg from './assets/instalacao_aquecedor.jpg';
 import manutencaoImg from './assets/manutencao_aquecedor.jpg';
 import consertoImg from './assets/conserto_aquecedor.jpg';
-import heroBackground from './assets/hero_background.jpg';
+import heroBackground from './assets/new_hero_background.png';
 
 import ServicesPage from './pages/Services';
 
